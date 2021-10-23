fa1c4639f1d6f89b86f6a1496b4d5985bd5c1511
j0s34m1g0 <68119912+j0s34m1g0@users.noreply.github.com>
2021-10-23 14:19:21 -0500

    make it better

--- src/index.js
+++ src/index.js
@@ -3,8 +3,7 @@ const app = express();
 const morgan = require("morgan");
 const path = require("path");
 const cors = require("cors");
-
-const { mongoose } = require("./database");
+const { mongoose } = require("../api/database");
 
 // Settings
 app.set("port", process.env.PORT || 3000);
@@ -15,8 +14,8 @@ app.use(express.json());
 app.use(cors());
 
 // Routes
-app.use("/api/products", require("./routes/products.routes"));
-app.use("/api/ventas", require("./routes/ventas.routes"));
+app.use("/api/products", require("../api/routes/products.routes"));
+app.use("/api/ventas", require("../api/routes/ventas.routes"));
 
 // Static files
 app.use(express.static(path.join(__dirname, "public")));
@@ -28,6 +27,7 @@ app.get("/api/products", function (req, res, next) {
 app.get("/api/ventas", function (req, res, next) {
   res.json({ msg: "This is CORS-enabled for all origins!" });
 });
+
 app.listen(app.get('port'), function () {
   console.log("CORS-enabled web server listening on port 3000");
 });
